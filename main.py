0f737ef21713c1d30434bafddced0194071989a0
Solostov <vakavaka36@mail.ru>
2026-01-05 14:46:16 +0300

    Проверка ключа

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ from tkinter import ttk, scrolledtext, messagebox
 import binascii
 import re
 from typing import Tuple
+
+class SimpleKeyValidator:
+    MIN_LENGTH = 8
+    COMMON_PASSWORDS = {'password', '123456', 'qwerty', 'admin'}
+    
+    @staticmethod
+    def validate_key(key: str) -> tuple:
+        if not key:
+            return False, 'Ключ не может быть пустым', 0
+        if len(key) < SimpleKeyValidator.MIN_LENGTH:
+            return False, f'Ключ должен быть не менее {MIN_LENGTH} символов', 0
